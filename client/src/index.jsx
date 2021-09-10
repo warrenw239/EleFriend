f9164161066ecbdfcf9af122c56b9078ab7892f0
Warren Williams <warrenw239@gmail.com>
2021-09-10 19:50:02 -0400

    fixing error in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App.jsx';
 
-ReactDOM.render(<App style={{position: 'center'}}/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
 
 
 
